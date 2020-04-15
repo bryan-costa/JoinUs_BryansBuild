febb64fb76914d802c140ef51fb83a819655d7bb
The_Costa <bryan.costa@roadrunner.com>
2020-04-15 16:02:49 -0700

    New Insert Command

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("Hello World!")
+var { connection } = require('./connection.js')
+var mysql = require('mysql');
+const { internet, date } = require('faker')
+
+var person = {
+    email: internet.email(),
+    created_at: date.past()
+};
+let q = 'SELECT * FROM users ORDER BY created_at DESC'
+
+var end_result = connection.query('INSERT INTO users SET ?', person, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+});
+
+connection.query(q, function (err, result) {
+    if (err) throw err;
+    console.log(result);
+});
+
+connection.end();
